635ee327e4e24baa4b343a4c13f7e014fc29c02e
ryotaKFC <ryotamokatoka@gmail.com>
2025-09-20 19:46:28 +0900

    ロビーパネルの作成中

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -4,30 +4,36 @@ import { Navigation } from "@/components/navigation";
 import { Button } from "@/components/ui/button";
 import { useLobbies } from "./hooks/useLobbies"
 import Link from "next/link";
+import Card from "@/components/ui/card";
 
 
 
 export default function Lobbies() {
     const {lobbies} = useLobbies();
 
-
-
     return (
-        <main>
+        <>
             <Navigation/>
-            <h1>Lobby一覧</h1>
-            <Link href="/lobby/create">
-                <Button type="button">ロビーの作成</Button>
-            </Link>
-            <ul>
-                {lobbies.map((lobby) => (
-                    <li key={lobby.id}>
-                        <a href={`/lobby/${lobby.id}`} className="">{lobby.name}</a>
-                        <div className="">{new Date(lobby.startTime).toLocaleString()}</div>
-                    </li>
-                ))}
-            </ul>
-        </main>
+            <main>
+                <div className="mx-auto my-5 text-center">
+                <Link href="/lobby/create">
+                    <Button type="button" size={"lg"} >ロビーの作成</Button>
+                </Link>
+                <h1 className="font-bold m-4 text-center text-3xl sm:text-5xl">Lobbies List</h1>
+
+                </div>
+                <div className="px-4 grid grid-cols-1 sm:px-8 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                    {lobbies.map((lobby) => (
+                        <Card key={lobby.id} className="mx-5 p-12 text-center bg-white">
+                            <Link href={`/lobby/${lobby.id}`} className="text-xl sm:text-2xl">
+                                {lobby.name}
+                            </Link>
+                            <div className="">{new Date(lobby.startTime).toLocaleString()}</div>
+                        </Card>
+                    ))}
+                </div>
+            </main>
+        </>
         
     )
 }
