4960b2c7c6a2cdd65bb362467d3b38f12a510403
ryotaKFC <ryotamokatoka@gmail.com>
2025-09-25 11:37:36 +0900

    スマホ版での見た目整え

--- src/app/lobby/create/page.tsx
+++ src/app/lobby/create/page.tsx
@@ -11,11 +11,11 @@ export default async function Page( {searchParams}: { searchParams: Promise<{[ke
             <Navigation />
             <main>
                 <h1 className="font-bold text-3xl text-center m-5">ロビーの作成</h1>
-                <Card variant="background" className="mx-[3%] px-10 sm:mx-[10%]">
+                <Card variant="background" className="mx-[3%] px-4 sm:mx-[10%] sm:px-10">
                     <Form isPrivateParam={isPrivate === "true"} />
                 </Card>
 
-                <Card variant="background" className="mx-[3%] px-10 sm:mx-[10%]">
+                <Card variant="background" className="mx-[3%] px-4 sm:mx-[10%] sm:px-10">
                     <h2 className="font-bold text-xl text-center">ロケーションの選択</h2>
                     <MapContent />
                 </Card>
