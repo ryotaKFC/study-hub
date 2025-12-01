2ede5cb407ce18c14c06e662a50b7d78480becd3
ryotaKFC <ryotamokatoka@gmail.com>
2025-12-01 20:02:35 +0900

    ロード画面実装

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import { Navigation } from "@/components/navigation";
+
+export default function Loading() {
+	return (
+		<>
+			<Navigation />
+			<p className="text-2xl text-center">Loading...</p>;
+		</>
+	);
+}
