31a689964a1ab939e556653e40bcb70f08ac63f6
ryotaKFC <ryotamokatoka@gmail.com>
2025-12-03 11:50:14 +0900

    refactor: loading表示のためPromise型受け渡す感じにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { LobbyDetail } from "@/features/lobby/components/lobby-detail";
 import { LobbyProvider } from "@/features/lobby/providers/lobby-provider";
 import { Lobby } from "@/features/lobby/types";
 
-const previewLobby: Lobby = {
+const previewLobbyPromise: Promise<Lobby> = Promise.resolve({
 	lobbyId: "prev",
 	lobbyName: "テストロビー",
 	startTime: "2025-10-03T04:17:37.354Z",
@@ -14,13 +14,13 @@ const previewLobby: Lobby = {
 	memberCount: 0,
 	lastActivityAt: "",
 	isInSchool: false,
-};
+});
 
 export default function Page() {
 	return (
 		<div className="relative min-h-screen">
 			<div className="absolute inset-0 mt-7 -z-10 opacity-60 pointer-events-none blur-sm overflow-hidden">
-				<LobbyProvider lobby={previewLobby} previewMode={true}>
+				<LobbyProvider lobbyPromise={previewLobbyPromise} previewMode={true}>
 					<LobbyDetail />
 				</LobbyProvider>
 			</div>
