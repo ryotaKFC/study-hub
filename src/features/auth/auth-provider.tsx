da1b2f8b7f6b08123a230a0e079695fe4435eb80
ryotaKFC <ryotamokatoka@gmail.com>
2026-07-08 13:33:30 +0900

    feat: 認可コードの交換をクライアント→サーバーに

--- src/features/auth/auth-provider.tsx
+++ src/features/auth/auth-provider.tsx
@@ -38,10 +38,11 @@ export function AuthProviders({ children }: Props) {
 	}, [supabaseClient.auth]);
 
 	async function signInWithGoogle() {
+		console.log(location.origin);
 		const { error } = await supabaseClient.auth.signInWithOAuth({
 			provider: "google",
 			options: {
-				redirectTo: window.location.href, // 直前のURLに戻す
+				redirectTo: `${location.origin}/auth/callback`,
 			},
 		});
 		if (error) {
